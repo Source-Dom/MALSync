09a0bd2690e9d114991dcc502d6331bd13afe619
Source-Dom <84047066+Source-Dom@users.noreply.github.com>
2025-05-24 05:16:40 +0000

    Auto Urls

--- src/pages/playerUrls.js
+++ src/pages/playerUrls.js
@@ -526,6 +526,7 @@ module.exports = {
       '*://sarahnewspaperbeat.com/e/*',
       '*://diananatureforeign.com/e/*',
       '*://jonathansociallike.com/e/*',
+      '*://jilliandescribecompany.com/e/*',
       // auto-voe-replace-dont-remove
     ],
   },
